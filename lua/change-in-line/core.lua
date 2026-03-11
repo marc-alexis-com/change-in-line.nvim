a6ed0527d039d0e29dec75106fb9018373efa285
Marc-Alexis-com <contact@marc-alexis.com>
2026-03-11 09:08:44 +0100

    FEAT: add label mode for multiple quote pairs

--- lua/change-in-line/core.lua
+++ lua/change-in-line/core.lua
@@ -23,30 +23,4 @@ function M.find_quote_pairs(line)
 	return pairs
 end
 
-function M.find_closest_pair(pairs, cursor_col)
-	local best_pair = nil
-	local best_dist = math.huge
-
-	if #pairs == 0 then
-		return nil
-	end
-
-	for i = 1, #pairs do
-		if pairs[i][1] <= cursor_col and pairs[i][2] >= cursor_col then
-			return pairs[i]
-		else
-			local a = math.abs(cursor_col - pairs[i][1])
-			local b = math.abs(cursor_col - pairs[i][2])
-
-			local dist = math.min(a, b)
-			if dist < best_dist then
-				best_dist = dist
-				best_pair = pairs[i]
-			end
-		end
-	end
-
-	return best_pair
-end
-
 return M
--- lua/change-in-line/init.lua
+++ lua/change-in-line/init.lua
@@ -4,23 +4,69 @@ function M.setup(opts)
 	M.opts = opts or {}
 end
 
-function M.change_in_quotes(action)
-	local core = require("change-in-line.core")
-	local row, col = unpack(vim.api.nvim_win_get_cursor(0))
-	col = col + 1 -- col is 0-based so add 1 to fit lua's
+local ns = vim.api.nvim_create_namespace("change-in-line")
 
-	local line = vim.api.nvim_get_current_line()
-	local closest_pair = core.find_closest_pair(core.find_quote_pairs(line), col)
 
-	if closest_pair == nil then
-		vim.notify("No quote pair found :(", vim.log.levels.WARN)
-		return
+local function show_labels(pairs, row, action)
+	for i = 1, #pairs do
+		local mid         = math.floor((pairs[i][1] + pairs[i][2]) / 2)
+		local label_left  = mid - pairs[i][1] - 1
+		local label_right = pairs[i][2] - mid - 1
+		local label_text  = string.rep("~", label_left) .. tostring(i) ..
+			string.rep("~", label_right)
+
+		vim.api.nvim_buf_set_extmark(0, ns, row - 1, pairs[i][1], {
+			virt_text = { { label_text, "WarningMsg" } },
+			virt_text_pos = "overlay",
+		})
 	end
 
-	vim.api.nvim_win_set_cursor(0, { row, closest_pair[1] - 1 }) -- match neovim 0-based collumn
+	vim.cmd("redraw")
+
+	local choice = nil
+	while choice == nil do
+		local code = vim.fn.getchar()
+		if code == 27 or code == 58 then -- escape and : key ascii value
+			vim.api.nvim_buf_clear_namespace(0, ns, 0, -1)
+			return
+		end
+
+		local char = vim.fn.nr2char(code)
+		local index = tonumber(char)
+
+		if index and index >= 1 and index <= #pairs then
+			choice = index
+		end
+	end
+
+	local pair = pairs[choice]
+	vim.api.nvim_win_set_cursor(0, { row, pair[1] - 1 }) -- match neovim 0-based collumn
+
+	vim.api.nvim_buf_clear_namespace(0, ns, 0, -1)
 
 	local keys = vim.api.nvim_replace_termcodes('c' .. action .. '"', true, false, true)
 	vim.api.nvim_feedkeys(keys, 'n', false)
+end
+
+function M.change_in_quotes(action)
+	local core = require("change-in-line.core")
+	local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+	col = col + 1 -- col is 0-based so add 1 to fit lua's array
+
+	local line = vim.api.nvim_get_current_line()
+	local pairs = core.find_quote_pairs(line)
+
+	if #pairs == 0 then
+		vim.notify("No \" pair found :(", vim.log.levels.WARN)
+		return
+	elseif #pairs == 1 then
+		vim.api.nvim_win_set_cursor(0, { row, pairs[1][1] - 1 }) -- match neovim 0-based collumn
+
+		local keys = vim.api.nvim_replace_termcodes('c' .. action .. '"', true, false, true)
+		vim.api.nvim_feedkeys(keys, 'n', false)
+	else
+		show_labels(pairs, row, action)
+	end
 end
 
 return M
